2153fd6ae4b60961658e6f1a98a17c2cf83a1df7
prof1k <prof1k@prof1kUbuntu>
2017-12-14 17:08:43 +0700

    Add video capture and recognize video

--- Recognition/src/Recognition.cpp
+++ Recognition/src/Recognition.cpp
@@ -21,7 +21,87 @@ using namespace std;
 
 alpr::Alpr openalpr("ru", "./config/openalpr.conf", "/usr/share/openalpr/runtime_data");
 
+void RecognitionPlate(cv::Mat plate)
+{
+        if (openalpr.isLoaded())
+        {
+        imshow("Input", plate);
+        openalpr.setTopN(5);
+        //openalpr.setDefaultRegion("il");
+        std::vector<alpr::AlprRegionOfInterest> regionsOfInterest;
+        regionsOfInterest.push_back(alpr::AlprRegionOfInterest(0, 0, plate.cols, plate.rows));
+        alpr::AlprResults results = openalpr.recognize(plate.data, plate.elemSize(), plate.cols, plate.rows, regionsOfInterest);
+        // cout << results.plates.size() << endl;
+        for (int i = 0; i < results.plates.size(); i++)
+        {
+           alpr::AlprPlateResult plate = results.plates[i];
+           std::cout << "plate" << i << ": " << plate.topNPlates.size() << " results" << std::endl;
+
+             for (int k = 0; k < plate.topNPlates.size(); k++)
+             {
+               alpr::AlprPlate candidate = plate.topNPlates[k];
+               std::cout << "    - " << candidate.characters << "\t confidence: " << candidate.overall_confidence;
+               std::cout << "\t pattern_match: " << candidate.matches_template << std::endl;
+             }
+        }
+        }
+        else
+        {
+                std::cerr << "Fail load openalpr";
+        }
+}
+
+void CameraAndDetect()
+{
+        cv::VideoCapture cap;
+//      cap.set(CV_CAP_PROP_FOURCC, CV_FOURCC('M','J','P','G'));
+
+
+        //cap.open(0);
+        //cap.open("./MOVI0005.avi");
+        cap.open("./ch5.avi");
+
+
+//      cap.set(CV_CAP_PROP_FOURCC, CV_FOURCC('M','J','P','G'));
+//      cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+//      cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+        //cap.set(CV_CAP_PROP_FPS, 5);
+        //cv::namedWindow("MyWindow", CV_WINDOW_AUTOSIZE);
+
+        int fps=0;
+
+        while (1)
+        {
+                cv::Mat frame;
+                bool bSuccess = cap.read(frame);
+                if (!bSuccess)
+                {
+                        cout << "Cannot read" << endl;
+                        break;
+                }
+                fps += cap.get(CV_CAP_PROP_FPS);
+                //cout << fps << endl;
+//              cv::flip(frame, frame, 1);
+                //imshow("MyWindow", frame);
+                if (fps > 40 )
+                {
+                        cap >> frame;
+                        //if(cascade.load(cascadeName))
+                        RecognitionPlate(frame);
+                        //DetectAndCut(frame,cascade,scale,tryflip);
+                        fps = 0;
+                        frame.release();
+                }
+                if (cvWaitKey(30) == 27)
+                {
+                        cap.release();
+                        break;
+                }
+        }
+}
+
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	CameraAndDetect();
 	return 0;
 }
